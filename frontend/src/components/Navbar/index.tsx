39969906d6d0101a6874fdfedc15c8d4021002dc
EltonFelix <eltomfelix@gmail.com>
2022-01-12 10:40:00 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg'; //importando imagem
+import './styles.css';
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/elton-felix">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/elton-felix</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar; //exportando a função para usar em outro componente
