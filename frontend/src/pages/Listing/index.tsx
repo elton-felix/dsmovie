2c7b986159931b5e8867cddf7ce8d4583e701495
EltonFelix <eltomfelix@gmail.com>
2022-01-14 10:24:59 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
@@ -9,38 +9,41 @@ function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0);//userstate um hook que mantem o estado do componente , para q ele não seja renderizado mais de uma vez no ciclo de vida do react
 
-    useEffect(() =>{ //usereffect só executará essa função quando carregar o componente
-        axios.get(`${BASE_URL}/movies?size=12&page=1`)//ultizamos o .then para executar algo depois da resposta chegar
-        .then(response => {
-           const data = response.data as MoviePage;
-           console.log(data);//data é o corpo da resposta
-           setPageNumber(data.number);
-            
-        });
-    }, []);
+    //estamos definindo que o tipo de esta é <MoviePage>
+    const [page, setPage] = useState<MoviePage>({//esse estado guarda a pagina que foi buscada na requisição
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
+
+    useEffect(() => { //usereffect só executará essa função quando carregar o componente
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)//ultizamos o .then para executar algo depois da resposta chegar
+            .then(response => {
+                const data = response.data as MoviePage;//pega o corpo da resposta como sendo um object MoviePage
+                setPage(data);//salvando a pagina que voltou da requisição
+
+
+            });
+    }, [pageNumber]);
 
     return (
         <>
-        <p>{pageNumber}</p>
             <Pagination />
-
+            
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => (//.map permite que façamos operações em cada elemento da coleção , nesse caso de cada filme
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
                 </div>
             </div>
         </>
